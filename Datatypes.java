4adc6cf75814fb5be187e1256538c8967b033695
mdakram-07 <mdakram62169@gmail.com>
2025-11-12 22:52:14 +0530

    commit

---
+++ Datatypes.java
@@ -0,0 +1,21 @@
+public class Datatypes {
+    public static void main(String[] args) {
+        // Primitive Data Types
+        int num = 5;               // Integer
+        float salary = 5.99f;   // Floating point number
+        char ch = 'D';        // Character
+        boolean a = true;      // Boolean
+        double myDoubleNum = 19.99; // Double precision floating point number
+
+        // Non-Primitive Data Types
+        String msg = "Hello";    // String
+
+        // Displaying the values
+        System.out.println("Integer: " + num);
+        System.out.println("Float: " + salary);
+        System.out.println("Character: " + ch);
+        System.out.println("Boolean: " + a);
+        System.out.println("Double: " + myDoubleNum);
+        System.out.println("String: " + msg);
+    }
+}
